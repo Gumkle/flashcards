8246ef41b07bfa11eeeb3acd8328b9eb55087df0
Dawid Dziedzic <dziedzicdev@gmail.com>
2019-01-11 00:06:35 +0100

    Added routes cuz why not

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::get('flashcards', 'Api\FlashcardsController@index')->name('flashcards.index');
+Route::get('flashcards\{flashcard`}', 'Api\FlashcardsController@show')->name('flashcards.show');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
 });
