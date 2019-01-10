004b15bd01fef18857d4b6826815162d0e90f440
Dawid Dziedzic <dziedzicdev@gmail.com>
2019-01-11 00:09:31 +0100

    Some more routes

--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,11 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::get('flashcards', 'Api\FlashcardsController@index')->name('flashcards.index');
-Route::get('flashcards\{flashcard`}', 'Api\FlashcardsController@show')->name('flashcards.show');
+Route::get('flashcards', 'Api\FlashcardController@index')->name('flashcards.index');
+Route::get('flashcards\{flashcard`}', 'Api\FlashcardController@show')->name('flashcards.show');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-
+    Route::post('flashcards', 'Api\FlashcardController@store')->name('flashcards.store');
+    Route::post('flashcards\{flashcard}', 'Api\FlashcardController@update')->name('flashcards.update');
+    Route::delete('flashcards\{flashcard}', 'Api\FlashcardController@delete')->name('flashcards.delete');
 });
